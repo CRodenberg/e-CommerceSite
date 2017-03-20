83cbbdf6dce93ea965aa01192ee98716f9cd4969
CRodenberg <crodenberg15@gmail.com>
2017-03-20 11:33:59 -0400

    Minor Update to header and Adding Page Content to index.php

--- index.php
+++ index.php
@@ -3,9 +3,35 @@
 require('./includes/config.inc.php');
 require(MYSQL);
 include('./includes/header.html');
+?>
 
+<div id="all">
 
-/*Page CONTENT FOR INDEX WILL GO RIGHT HERE*/
+        <div id="content">
 
+            <div class="container">
+                <div class="col-md-12">
+                    <div id="main-slider">
+                        <div class="item">
+                            <img src="img/main-slider1.jpg" alt="" class="img-responsive">
+                        </div>
+                        <div class="item">
+                            <img class="img-responsive" src="img/main-slider2.jpg" alt="">
+                        </div>
+                        <div class="item">
+                            <img class="img-responsive" src="img/main-slider3.jpg" alt="">
+                        </div>
+                        <div class="item">
+                            <img class="img-responsive" src="img/main-slider4.jpg" alt="">
+                        </div>
+                    </div>
+                    <!-- /#main-slider -->
+                </div>
+            </div>
+
+            <!-- *** ADVANTAGES END *** -->
+        </div>
+        <!-- /#content -->
+<?php
 include('/includes/footer.html');
 ?>
